45ff3d1a9397d7b2ad61c0d5062ab3f82e0a8eb3
Putu Kusmawan <putuyuwono@yahoo.com>
2019-02-12 20:37:00 +0900

    Initial commits

---
+++ MultiLangTest/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Globalization;
+using System.Windows;
+using WPFLocalizeExtension.Engine;
+
+namespace MultiLangTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int counter = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            counter++;
+
+            if (counter % 3 == 1)
+            {
+                //LocalizeDictionary.Instance.SetCurrentThreadCulture = true;
+                LocalizeDictionary.Instance.Culture = new CultureInfo("ja-JP");
+            }
+            else if (counter % 3 == 2)
+            {
+                //LocalizeDictionary.Instance.SetCurrentThreadCulture = true;
+                LocalizeDictionary.Instance.Culture = new CultureInfo("ko-KR");
+            }
+            else
+            {
+                //LocalizeDictionary.Instance.SetCurrentThreadCulture = true;
+                LocalizeDictionary.Instance.Culture = new CultureInfo("en");
+
+            }
+        }
+    }
+}
